c14fa40879bfda6ed282e3de97464eccd76b7f44
Christian Dueck <christian@dueck.com.br>
2021-03-08 12:26:23 -0300

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,7 +9,7 @@ export default class MyDocument extends Document {
                     <link rel="preconnect" href="https://fonts.gstatic.com" />
                     <link href="https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap" rel="stylesheet" />
 
-                    <meta property="og:image" content="./christian-dueck-profile.jpg" />
+                    <meta property="og:image" content="https://dueck.app/christian-dueck-profile.jpg" />
                     <meta property="og:image:type" content="image/jpeg" />
                     <meta property="og:image:width" content="728" />
                     <meta property="og:image:height" content="606" />
